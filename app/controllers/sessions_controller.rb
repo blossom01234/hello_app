f45ceff091ce96b07bc4a7397fba5ad32cca61ba
y.shimaubkuro <y.shimabukuro@gvc.co.jp>
2024-06-08 11:14:41 +0900

    8.17

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new', status: :unprocessable_entity
     end
   end
