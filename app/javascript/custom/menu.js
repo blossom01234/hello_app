2d3cca637e9d26ec4ef21e872f8d646de9bd1600
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-06-09 10:49:26 +0900

    9章終わり

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,18 +1,18 @@
 // メニュー操作
 
-//トグルリスナーを追加する
+// トグルリスナーを追加する
 function addToggleListener(selected_id, menu_id, toggle_class) {
   let selected_element = document.querySelector(`#${selected_id}`);
-  selected_element.addEventListener("click", function (event) {
+  if (!selected_element) return
+  selected_element.addEventListener("click", function(event) {
     event.preventDefault();
     let menu = document.querySelector(`#${menu_id}`);
-    menu.classList.toggle(toggle_class)
+    menu.classList.toggle(toggle_class);
   });
 }
 
-// トグルリスナーを追加してクリックをリッスンする
+// クリックをリッスンするトグルリスナーを追加する
 document.addEventListener("turbo:load", function() {
-  addToggleListener("hamburger", "navbar-menu", "collapse");
-  addToggleListener("account", "dropdown-menu", "active");
+  addToggleListener("hamburger", "navbar-menu",   "collapse");
+  addToggleListener("account",   "dropdown-menu", "active");
 });
-  
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,7 +17,7 @@ class InvalidPasswordTest < UsersLogin
   test "login with valid email/invalid password" do
     post login_path, params: { session: { email:    @user.email,
                                           password: "invalid" } }
-    assert_not logged_in?
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -37,7 +37,7 @@ end
 class ValidLoginTest < ValidLogin
 
   test "valid login" do
-    assert logged_in?
+    assert is_logged_in?
     assert_redirected_to @user
   end
 
@@ -61,7 +61,7 @@ end
 class LogoutTest < Logout
 
   test "successful logout" do
-    assert_not logged_in?
+    assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
   end
@@ -71,5 +71,30 @@ class LogoutTest < Logout
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
